6f21966e85e6f085ec8bccecea2e67b0be8af584
RyanMcgrane <c16419862@mydit.ie>
2018-03-13 16:33:11 +0000

    Initial commit

---
+++ Assets/playerCollision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class playerCollision : MonoBehaviour {
+
+    public playerMov movement;
+
+    void OnCollisionEnter( Collision collisionInfo)
+    {
+        if ( collisionInfo.collider.tag == "Obstacle" )
+        {
+
+            movement.enabled = false;
+        }
+    }
+}
